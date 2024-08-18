14eaaee805d2fbae0868f277045e28db9d7d0f40
zshnb <a857681664@gmail.com>
2024-08-18 14:01:35 +0800

    chore: integrate pino-log

---
+++ src/logger.ts
@@ -0,0 +1,22 @@
+import pino from 'pino';
+import '@dotenvx/dotenvx';
+
+console.log(process.env);
+const transport =
+  process.env.NODE_ENV === 'development'
+    ? {
+        target: 'pino-pretty',
+      }
+    : undefined;
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport,
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
+
+export default logger;
